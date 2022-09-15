d6f03067112042b46b32ae52be3d0d66b6d9c015
Laura Puckoriute <laura.puckoriute@kainos.com>
2022-09-15 10:33:44 +0100

    createemployee routing

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,17 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.get('/createemployee', (req, res) => { 
+    res.render('createemployee', { } ); 
+  });
+  
+router.post('/createemployee', async (req, res) => { 
+    var employee = req.body 
+  
+    // todo: validate here 
+      await companydata.createEmployee(employee.body) 
+      res.render('getemployees', { cities: await companydata.getEmployees()});
+  })
+
+
 module.exports = router
